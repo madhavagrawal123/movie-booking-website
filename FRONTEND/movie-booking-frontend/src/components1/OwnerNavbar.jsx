b1857ef1d88b166eec10d1de0427375d8ab0314b
Madhav Agrawal <madhavagrawal2023@gmail.com>
2026-07-13 19:59:36 +0530

    added browser movie navigate btn

--- FRONTEND/movie-booking-frontend/src/components1/OwnerNavbar.jsx
+++ FRONTEND/movie-booking-frontend/src/components1/OwnerNavbar.jsx
@@ -24,12 +24,18 @@ function OwnerNavbar() {
     };
 
     return (
+    <div className="h-16 bg-zinc-900 border-b border-red-700 flex items-center px-8">
+        <h2 className="text-white text-xl font-semibold">
+            Theatre Owner Panel
+        </h2>
 
-        <div className="h-16 bg-zinc-900 border-b border-red-700 flex justify-between items-center px-8">
-
-            <h2 className="text-white text-xl font-semibold">
-                Theatre Owner Panel
-            </h2>
+        <div className="flex items-center gap-3 ml-auto">
+            <button
+                onClick={() => navigate("/")}
+                className="bg-red-600 hover:bg-red-700 px-5 py-2 rounded-lg"
+            >
+                Browse Movies
+            </button>
 
             <button
                 onClick={handleLogout}
@@ -37,10 +43,9 @@ function OwnerNavbar() {
             >
                 Logout
             </button>
-
         </div>
-
-    );
+    </div>
+);
 
 }
 
